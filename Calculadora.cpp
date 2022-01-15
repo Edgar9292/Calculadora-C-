5ceb4e6294cf1b36ba41e778410aaa3e28756b80
Edgar9292 <92225390+Edgar9292@users.noreply.github.com>
2022-01-15 17:14:03 -0300

    Add files via upload

---
+++ Calculadora.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+float num1 ,num2;
+cout <<"primeiro numero:";
+cin >> num1;
+cout << "segundo numero";
+cin >> num2;
+
+cout <<"soma:" << num1 + num2 << endl;
+cout <<"subtracao:" << num1 - num2 <<endl;
+cout <<"multiplicacao" << num1*num2 <<endl;
+cout <<"divisao" << num1/num2 <<endl;
+return 0;
+}
